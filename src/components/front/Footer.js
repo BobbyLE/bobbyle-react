6ec1c92957b45706decdf8bc4b94c8e77b8dc98e
Bobby <bobby.weiss.le@gmail.com>
2018-02-22 14:20:10 +0100

    Update Footer

--- src/components/front/Footer.js
+++ src/components/front/Footer.js
@@ -1,23 +1,48 @@
 import React from 'react';
 
 
-const Footer = () => (
-  <footer className="footer">
-    <section className="footer__copyright">
-      <span>Copyright © Bobby Le.</span>
-    </section>
-    <aside className="footer__links">
-      <ul className="footer__links__list">
-        <li><a href="#">Fbk</a></li>
-        <li><a href="#">Lin</a></li>
-        <li><a href="#">Git</a></li>
-        <li><a href="#">Insta</a></li>
-        <li><a href="#">500px</a></li>
-        <li><a href="#">Flickr</a></li>
-        <li><a href="#">Twit</a></li>
-      </ul>
-    </aside>
-  </footer>
-)
+const Footer = () => {
+  const socialUrl = {
+    facebook: "https://www.facebook.com/bobby.le",
+    linkedin: "https://www.linkedin.com/in/bobbywle/",
+    github: "https://github.com/bobbyle",
+    instagram: "https://www.instagram.com/bobbywle/",
+    px: "http://500px.com/bobbyle",
+    flickr: "https://www.flickr.com/photos/bobbyle",
+    twitter: "https://twitter.com/BobbyWeissLE"
+  }
+  return (
+    <footer className="footer">
+      <section className="footer__copyright">
+        <span>Copyright © Bobby Le.</span>
+      </section>
+      <aside className="footer__links">
+        <ul className="footer__links__list">
+          <li className="footer__links__list__item">
+            <a href={socialUrl.facebook} target="_blank" rel="external" className="footer__links__social footer__links__social--facebook"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.linkedin} target="_blank" rel="external" className="footer__links__social footer__links__social--linkedin"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.github} target="_blank" rel="external" className="footer__links__social footer__links__social--github"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.instagram} target="_blank" rel="external" className="footer__links__social footer__links__social--instagram"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.px} target="_blank" rel="external" className="footer__links__social footer__links__social--500px"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.flickr} target="_blank" rel="external" className="footer__links__social footer__links__social--flickr"></a>
+          </li>
+          <li className="footer__links__list__item">
+            <a href={socialUrl.twitter} target="_blank" rel="external" className="footer__links__social footer__links__social--twitter"></a>
+          </li>
+        </ul>
+      </aside>
+    </footer>
+  );
+};
 
 export default Footer;
