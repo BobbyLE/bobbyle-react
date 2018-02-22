74a8cfb05f56015dda761aba9a23e16f6019468b
Bobby <bobby.weiss.le@gmail.com>
2018-02-22 16:45:04 +0100

    Add AboutPage with main

--- src/components/front/Header.js
+++ src/components/front/Header.js
@@ -2,51 +2,84 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import GradientNavLink from './GradientNavLink';
 
-const Header = () => (
-  <header className="header">
-    <section className="header__logo">
-      <h1>Bobby Le</h1>
-    </section>
-    <nav className="header__navbar">
-      <ul className="header__nav">
-        <li className="header__nav__item">
-          <GradientNavLink 
-            to="/about" 
-            activeClassName="is-selected"
-            beginColor="#009cf3"
-            endColor="#16d6d9">
-            About
-          </GradientNavLink>
-        </li>
-        <li className="header__nav__item">
-          <GradientNavLink 
-            to="/work" 
-            activeClassName="is-selected"
-            beginColor="rgb(255, 191, 2)"
-            endColor="rgb(255, 85, 25)">
-            Work
-          </GradientNavLink>
-        </li>
-        <li className="header__nav__item">
-          <a href="#">Photography
-          <div className="underline-style"></div>
-          </a>
-        </li>
-        <li className="header__nav__item">
-          <GradientNavLink 
-            to="/blog" 
-            activeClassName="is-selected"
-            beginColor="#16D6D9"
-            endColor="#96CC29">
-            Blog
-          </GradientNavLink>
-        </li>
-        <li className="header__nav__item">
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const aboutColor = {
+    beginColor: "#004792",
+    endColor: "#0097d7"
+  };
+  const workColor = {
+    beginColor: "#397f00",
+    endColor: "#9ecc00"
+  };
+  const photoColor = {
+    beginColor: "#e16000",
+    endColor: "#c89600"
+  };
+  const blogColor = {
+    beginColor: "#e60c00",
+    endColor: "#c34d00"
+  };
+  const contactColor = {
+    beginColor: "#a20330",
+    endColor: "#e600b3"
+  };
+
+  return (
+    <header className="header">
+      <section className="header__logo">
+        <h1>Bobby Le</h1>
+      </section>
+      <nav className="header__navbar">
+        <ul className="header__nav">
+          <li className="header__nav__item">
+            <GradientNavLink 
+              to="/about" 
+              activeClassName="is-selected"
+              beginColor={aboutColor.beginColor}
+              endColor={aboutColor.endColor}>
+              About
+            </GradientNavLink>
+          </li>
+          <li className="header__nav__item">
+            <GradientNavLink 
+              to="/work" 
+              activeClassName="is-selected"
+              beginColor={workColor.beginColor}
+              endColor={workColor.endColor}>
+              Work
+            </GradientNavLink>
+          </li>
+          <li className="header__nav__item">
+            <GradientNavLink 
+              to="#" 
+              activeClassName="is-selected"
+              beginColor={photoColor.beginColor}
+              endColor={photoColor.endColor}>
+              Photo
+            </GradientNavLink>
+          </li>
+          <li className="header__nav__item">
+            <GradientNavLink 
+              to="/blog" 
+              activeClassName="is-selected"
+              beginColor={blogColor.beginColor}
+              endColor={blogColor.endColor}>
+              Blog
+            </GradientNavLink>
+          </li>
+          <li className="header__nav__item">
+            <GradientNavLink 
+              to="#" 
+              activeClassName="is-selected"
+              beginColor={contactColor.beginColor}
+              endColor={contactColor.endColor}>
+              Contact
+            </GradientNavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default Header;
--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import GradientTitle from '../front/GradientTitle';
 
 
 const AboutPage = () => {
-  const beginColor = "#009cf3";
-  const endColor = "#16d6d9";
+  const beginColor = "#004792";
+  const endColor = "#0097d7";
   return (
     <div className="container main about">
       <section className="main__header column">
@@ -14,21 +15,30 @@ const AboutPage = () => {
           About
         </GradientTitle>
       </section>
-      <section className="presentation column small-12">
+      <section className="presentation collapse">
         <aside className="presentation__photo">
+          <img src="/images/photo.jpg" alt="photo" />
         </aside>
         <section className="presentation__text">
           <h2 className="presentation__text__title">Bobby Le</h2>
-          <span className="presentation__text__subtitle">Web Developer, Photographer</span>
-          <h3>Presentation</h3>
-          <p></p>
+          <span className="presentation__text__subtitle">
+            Web Developer, Photographer
+            <div className="underline-style"></div>
+          </span>
+          <h3 className="section-title">Presentation</h3>
+          <p>Everything started with passion to web technologies (HTML, Flash) at the age of 13 coding my first websites.
+          In 2012, I graduated with a <strong>Degree in Web Development and Multimedia</strong> in Paris, France. 
+          I now have 5 years experience working as a <strong>front-end developer</strong> in several web agencies.
+          <br />Right now, I am specialising in Javascript frameworks like <strong>ReactJS</strong>.</p>
+          <p>This website has been coded using ReactJS, Foundation, Yarn, Webpack, Babel & Redux. </p>
+          <p>When I'm not coding, you'll find me taking <Link to="#">photos</Link> or playing Volley-Ball & Guitar.</p>
         </section>
       </section>
       <section className="experiences column small-12 medium-6">
-        <h3>Experiences</h3>
+        <h3 className="section-title">Experiences</h3>
       </section>
       <section className="skills column small-12 medium-6">
-        <h3>Skills</h3>
+        <h3 className="section-title">Skills</h3>
       </section>
     </div>   
   )
