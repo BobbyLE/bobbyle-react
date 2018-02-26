b12ba154b13a02dc27e6a0c4bb5136e0a66bb50c
Bobby <bobby.weiss.le@gmail.com>
2018-02-26 13:38:13 +0100

    Added resume pdf

---
+++ src/tests/components/pages/AboutPage.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AboutPage from '../../../components/pages/AboutPage';
+
+test('should correctly render AboutPage', () => {
+  const wrapper = shallow(<AboutPage />);
+  expect(wrapper).toMatchSnapshot();
+});
