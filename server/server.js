cc821996cdc3d08f33dbe0f7b36726d67586fe12
Bobby Le <bobby.weiss.le@gmail.com>
2021-10-25 17:45:02 +0200

    chore(server): bump axios express nodemailer

--- server/server.js
+++ server/server.js
@@ -6,34 +6,76 @@ if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.development' });
 }
 
-const path = require('path'),
-      express = require('express'),
-      app = express(),
-      publicPath = path.join(__dirname, '..', 'public'),
-      port = process.env.PORT || 3000,
-      nodeMailer = require('nodemailer'),
-      bodyParser = require('body-parser'),
-      emailTo = 'bobbywle7@gmail.com'
+const path = require('path')
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+const app = express()
+const nodemailer = require('nodemailer')
+const { google } = require("googleapis")
+
+const publicPath = path.join(__dirname, '..', 'public')
+const port = process.env.PORT || 3000
+const emailTo = 'bobbywle7@gmail.com'
+const OAuth2 = google.auth.OAuth2;
 
 app.use(express.static(publicPath));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+app.post('/send-email', asyncHandler(async(req, res) => {
+  const CLIENT_ID = process.env.GMAIL_CLIENT_ID
+  const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET
+  const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN
+  const EMAIL_USER = process.env.GMAIL_EMAIL_USER
+  
+  const createTransporter = async () => {
+    const myOAuth2Client = new OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+    )
+    
+    myOAuth2Client.setCredentials({
+      refresh_token: REFRESH_TOKEN
+    })
 
-app.post('/send-email', function (req, res) {
-  let transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.GMAIL_ACCOUNT,
-        pass: process.env.GMAIL_PASSWORD
+    const accessToken = await new Promise((resolve, reject) => {
+      myOAuth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject();
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: EMAIL_USER, //your gmail account you used to set the project up in google cloud console"
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken, //access token variable we defined earlier
+      }
+    })
+
+    return transporter
+  }
+
+  const sendEmail = async (emailOptions) => {
+    try {
+      let emailTransporter = await createTransporter();
+      return await emailTransporter.sendMail(emailOptions);
+    } catch(error){
+      console.log(error)
     }
-  });
+  }
+    
   let mailOptions = {
     from: req.body.cName, // sender address
     to: emailTo, // list of receivers
@@ -41,12 +83,9 @@ app.post('/send-email', function (req, res) {
     text: req.body.cMessage, // plain text body
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-  });
+
+  const sendEmailToMe = await sendEmail(mailOptions)
+  console.log('Message %s sent: %s', sendEmailToMe.messageId, sendEmailToMe.response);
 
   let mailToSenderOptions = {
     from: 'Bobby Le', // sender address
@@ -54,15 +93,11 @@ app.post('/send-email', function (req, res) {
     subject: `Your message to ${emailTo} via Bobby Le Portfolio has been successfully sent`, // Subject line
     text: `Your message: ${req.body.cMessage}`, // plain text body
   }
-  transporter.sendMail(mailToSenderOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    res.sendFile(path.join(publicPath, 'index.html'));
-  });
-});
 
+  const sendEmailToSender = await sendEmail(mailToSenderOptions)
+  console.log('Message %s sent: %s', sendEmailToSender.messageId, sendEmailToSender.response)
+  res.sendFile(path.join(publicPath, 'index.html'))
+}))
 
 app.listen(port, () => {
   console.log('Server is up!');
